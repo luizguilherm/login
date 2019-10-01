2a50e46bdb70dac2f86885e5d02f58d6e8714b68
luizguilherm <luiz_gs_silva@estudante.sc.senai.br>
2019-09-30 21:54:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,10 +16,11 @@ abra a URL http//localhost/login -->
 
     <title>Sistema de login</title>
     <style>
-    #alerta, #caixaSenha, #caixaRegistro{
-        display: none;
-    }
-    
+        #alerta,
+        #caixaSenha,
+        #caixaRegistro {
+            display: none;
+        }
     </style>
 </head>
 
@@ -153,15 +154,12 @@ abra a URL http//localhost/login -->
                         <div class="form-group">
                             <input type="password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha">
 
-
                         </div>
-
 
                         <div class="form-group">
                             <input type="password" name="senhaUsuarioConfirmar" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar senha">
 
                         </div>
-
                         <div class="form-group">
                             <div class="custom-control custom-checkbox">
                                 <input type="checkbox" name="concordar" id="concordar" class="custom-control-inpunt">
@@ -170,9 +168,6 @@ abra a URL http//localhost/login -->
                                 </label>
                             </div>
                         </div>
-
-
-
                     </div>
                     <div class="form-group">
                         <input type="submit" id="btnregistrar" class="btn btn-primary btn-block">
@@ -183,29 +178,31 @@ abra a URL http//localhost/login -->
                             <a href="#" id="btnjaRegistrado2">
                                 Entrar por aqui.
                             </a>
-
                         </p>
-
                     </div>
-
-
                 </form>
-
-
             </div>
-
-
-
         </section>
-
         <!-- final do formulario de cadastro de novos usuarios -->
-
-
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script>
+            //Codigo jquery para mostrar e ocultar os formularios
+            $(function() _ {
+                $("#btnEsqueci").click(function() {
+                    $("#caixaLogin").hide(); //Oucultar
+                    $("#caixaSenha").show(); //Mostrar
+                });
+                $("#btnRegistrarNovo").click(function() {
+                    $("#caixaSenha").hide(); //Oucultar gerar senha
+                    $("#caixaLogin").show();//Mostrar caixa login
+                });
+
+            });
+        </script>
 </body>
 
 </html>
