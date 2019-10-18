9015d9ff5589621bf58f1d867c3bae8ae8ad71fb
luizguilherm <luiz_gs_silva@estudante.sc.senai.br>
2019-10-18 19:53:08 -0300

    aula 7

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Out-2019 às 21:27
+-- Tempo de geração: 07-Out-2019 às 21:26
 -- Versão do servidor: 10.3.16-MariaDB
 -- versão do PHP: 7.3.7
 
@@ -24,6 +24,44 @@ SET time_zone = "+00:00";
 DROP DATABASE IF EXISTS `senai`;
 CREATE DATABASE IF NOT EXISTS `senai` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `senai`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `idUsuario` int(10) UNSIGNED NOT NULL,
+  `nomeCompleto` varchar(100) NOT NULL,
+  `nomeDoUsuario` varchar(45) NOT NULL,
+  `emailUsuario` varchar(45) NOT NULL,
+  `senhaDoUsuario` char(40) NOT NULL,
+  `dataCriado` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD UNIQUE KEY `nomeDoUsuario` (`nomeDoUsuario`),
+  ADD UNIQUE KEY `emailUsuario` (`emailUsuario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
